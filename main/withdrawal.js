719357e1946ab45d2259f67d7d6aed8cb0590976
Alex Lewis <alexlewis374@gmail.com>
2020-05-24 13:40:58 +0100

    Test - withdrawal initialises with the amount of the withdrawal

--- main/withdrawal.js
+++ main/withdrawal.js
@@ -1,2 +1,5 @@
 export default class Withdrawal {
+  constructor (amount) {
+    this.amount = amount
+  }
 }
--- main/withdrawal.unit.test.js
+++ main/withdrawal.unit.test.js
@@ -1,9 +1,13 @@
 import Withdrawal from './withdrawal'
 
-const withdrawal = new Withdrawal()
+const withdrawal = new Withdrawal(20)
 
 describe('withdrawal is initialised', () => {
   it('is defined', () => {
     expect(withdrawal).toBeDefined()
+  })
+
+  it('returns the amount to be withdrawn', () => {
+    expect(withdrawal.amount).toBe(20)
   })
 })
