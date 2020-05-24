9f3bde8aa1b80847a2c3a0cfb1a0e4ac56ddcdca
Alex Lewis <alexlewis374@gmail.com>
2020-05-24 13:26:54 +0100

    Reorganise Deposit tests into one describe block

--- main/deposit.unit.test.js
+++ main/deposit.unit.test.js
@@ -2,20 +2,17 @@ import Deposit from './deposit'
 
 const deposit = new Deposit(20, '10-01-2012')
 
-describe('deposit', () => {
+describe('deposit is initialised', () => {
   it('is defined', () => {
     expect(deposit).toBeDefined()
   })
-})
 
-describe('deposit has an amount', () => {
   it('returns the amount to be deposited', () => {
     expect(deposit.amount).toBe(20)
   })
-})
 
-describe('deposit has a date', () => {
   it('returns the date of the deposit', () => {
     expect(deposit.date).toBe('10-01-2012')
   })
 })
+
