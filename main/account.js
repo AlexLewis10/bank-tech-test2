4d341030b8bb5905c8ae7513353b137bcac88606
Alex Lewis <alexlewis374@gmail.com>
2020-06-15 16:54:08 +0100

    G: Balance updates when a withdrawal is made

--- main/account.js
+++ main/account.js
@@ -19,4 +19,8 @@ export default class Account {
   _updateBalance (amount) {
     this.balance += amount
   }
+
+  makeWithdrawal (amount, date) {
+    this.balance -= amount
+  }
 }
--- main/account.unit.test.js
+++ main/account.unit.test.js
@@ -12,15 +12,15 @@ describe('Balance', () => {
   })
 })
 
-describe('transaction history', () => {
+describe('Transaction history', () => {
   it('should have a transaction history array', () => {
     expect(account.transactionHistory).toStrictEqual([])
   })
 })
 
-describe('can make a deposit', () => {
-  it('returns an array with the deposit', () => {
-    account.makeDeposit(20, '10-01-2012', 20)
+describe('Can make a deposit', () => {
+  it('transaction is added to transaction history', () => {
+    account.makeDeposit(20, '10-01-2012')
     expect(account.transactionHistory).toStrictEqual([['Credit', 20, '10-01-2012', 20]])
   })
 })
@@ -29,5 +29,11 @@ describe('Balance is updated', () => {
   it('when a deposit is made the balance is updated', () => {
     account.makeDeposit(20, '10-01-2012')
     expect(account.balance).toStrictEqual(20)
+  })
+
+  it('when a withdrawal is made the balance it updated', () => {
+    account.makeDeposit(20, '10-01-2012')
+    account.makeWithdrawal(10)
+    expect(account.balance).toStrictEqual(10)
   })
 })
