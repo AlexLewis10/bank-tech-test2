d33a652ffec84333d94f00f9f063a9b0fa54bf9d
Alex Lewis <alexlewis374@gmail.com>
2020-06-16 15:47:33 +0100

    G: Statement, returns a statement with a credit

--- main/statement.js
+++ main/statement.js
@@ -3,7 +3,13 @@ export default class Statement {
     this.headerString = 'date || credit || debit || balance'
   }
 
-  _printStatement () {
-    return this.headerString
+  _printStatement (transactionHistory) {
+    let date = transactionHistory[0][0]
+    date = date.replace(/-/g, '/')
+    let creditAmount = transactionHistory[0][2]
+    creditAmount = creditAmount.toFixed(2)
+    let balance = transactionHistory[0][3]
+    balance = balance.toFixed(2)
+    return `${this.headerString}\n ${date} || ${creditAmount} || || ${balance}`
   }
 }
--- main/statement.unit.test.js
+++ main/statement.unit.test.js
@@ -8,6 +8,11 @@ beforeEach(() => {
 
 describe('Statement', () => {
   it('returns the first line of the statement', () => {
-    expect(statement._printStatement()).toMatch(/date || credit || debit || balance/)
+    expect(statement._printStatement([['10-01-2012', 'Credit', 1000, 1000]])).toMatch(/date || credit || debit || balance/)
+  })
+
+  it('returns a statement with a debit', () => {
+    expect(statement._printStatement([['10-01-2012', 'Credit', 1000, 1000]])).toStrictEqual('date || credit || debit || balance' +
+                                                                                            '\n 10/01/2012 || 1000.00 || || 1000.00')
   })
 })
