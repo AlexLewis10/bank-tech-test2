0589e7644611befef1f0f4332e75935d4e9e4bf6
Alex Lewis <alexlewis374@gmail.com>
2020-06-16 17:58:12 +0100

    Include test to meet full acceptance criteria

--- main/statement.unit.test.js
+++ main/statement.unit.test.js
@@ -26,4 +26,11 @@ describe('Statement', () => {
     expect(statement._printStatement([['14-01-2012', 'Debit', 500, 2500]])).toStrictEqual('date || credit || debit || balance' +
                                                                                           '\n14/01/2012 || || 500.00 || 2500.00')
   })
+
+  it('returns a statement with more than one credit', () => {
+    expect(statement._printStatement([['14-01-2012', 'Debit', 500, 2500], ['13-01-2012', 'Credit', 2000, 3000], ['10-01-2012', 'Credit', 1000, 1000]])).toStrictEqual('date || credit || debit || balance' +
+                                                                                                                                  '\n14/01/2012 || || 500.00 || 2500.00' +
+                                                                                                                                  '\n13/01/2012 || 2000.00 || || 3000.00' +
+                                                                                                                                  '\n10/01/2012 || 1000.00 || || 1000.00')
+  })
 })
