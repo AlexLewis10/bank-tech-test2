2fef277fb600668d411e5ee684c24a1f9c61a473
Alex Lewis <alexlewis374@gmail.com>
2020-07-06 14:47:02 +0100

    Move balance updates tests to top of file

--- test/unit/account.unit.test.js
+++ test/unit/account.unit.test.js
@@ -6,23 +6,6 @@ beforeEach(() => {
   account = new Account()
 })
 
-describe('Can make a deposit', () => {
-  it('transaction is added to transaction history', () => {
-    account.makeDeposit(20, '10-01-2012')
-
-    expect(account.transactionHistory).toStrictEqual([['10-01-2012', 'Credit', 20, 20]])
-  })
-})
-
-describe('Can make a withdrawal', () => {
-  it('transaction is added to transaction history in the correct order', () => {
-    account.makeDeposit(20, '10-01-2012')
-    account.makeWithdrawal(10, '11-10-2012')
-
-    expect(account.transactionHistory).toStrictEqual([['11-10-2012', 'Debit', 10, 10], ['10-01-2012', 'Credit', 20, 20]])
-  })
-})
-
 describe('Balance updates', () => {
   it('when a deposit is made', () => {
     account.makeDeposit(20, '10-01-2012')
@@ -42,6 +25,23 @@ describe('Balance updates', () => {
     account.makeWithdrawal(30, '11-01-2012')
 
     expect(account.balance).toStrictEqual(20)
+  })
+})
+
+describe('Can make a deposit', () => {
+  it('transaction is added to transaction history', () => {
+    account.makeDeposit(20, '10-01-2012')
+
+    expect(account.transactionHistory).toStrictEqual([['10-01-2012', 'Credit', 20, 20]])
+  })
+})
+
+describe('Can make a withdrawal', () => {
+  it('transaction is added to transaction history in the correct order', () => {
+    account.makeDeposit(20, '10-01-2012')
+    account.makeWithdrawal(10, '11-10-2012')
+
+    expect(account.transactionHistory).toStrictEqual([['11-10-2012', 'Debit', 10, 10], ['10-01-2012', 'Credit', 20, 20]])
   })
 })
 
